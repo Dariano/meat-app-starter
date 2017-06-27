8f38f570b84df4ab966eee0a11dc77273865118e
dariano <darianosoares@hotmail.com>
2017-06-27 00:23:52 -0300

    Adicionado novos components e suas rotas. menu, menu-item e shopping-cat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { Routes } from '@angular/router'
 
 import { AboutComponent } from './about/about.component';
@@ -7,5 +8,6 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 export const ROUTES: Routes = [
     { path: '',  component: HomeComponent},
     { path: 'restaurants',  component: RestaurantsComponent},
+    { path: 'restaurants/:id',  component: RestaurantDetailComponent},
     { path: 'about',  component: AboutComponent}
 ]
