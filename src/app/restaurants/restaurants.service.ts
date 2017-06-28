724b6ba923fd10ad151483e79736c67e498de05d
dariano <darianosoares@hotmail.com>
2017-06-27 23:19:47 -0300

    Trabalhando com pipe. Separando as reviews

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable'
-import  'rxjs/add/operator/map'
-import  'rxjs/add/operator/catch'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 import { MEAT_API } from '../app.api';
 import { ErrorHandler } from '../app.error-heandler';
 
@@ -15,6 +15,13 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]> {
         return this.http
             .get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviews(id: string): Observable<any> {
+        return this.http
+            .get(`${MEAT_API}/restaurants/${id}/reviews`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
